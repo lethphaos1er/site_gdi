f1d2523da318af554ab13ac87752785c587d2cc4
Cedric Catoul <catoul.cedric@protonmail.com>
2026-07-01 21:53:58 +0200

    feat: create front entry point

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
+use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('home');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
