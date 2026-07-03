3d2cf3f97f60b2cafcf8ed7c376b4fb2286102e9
Cedric Catoul <catoul.cedric@protonmail.com>
2026-07-03 22:07:46 +0200

    feat: add auth navigation to store header

--- resources/js/Components/Front/StoreSelection/StoreHeader.vue
+++ resources/js/Components/Front/StoreSelection/StoreHeader.vue
@@ -1,7 +1,15 @@
+<script setup>
+import StoreHeaderNav from './StoreHeaderNav.vue';
+</script>
+
 <template>
     <header class="store-header">
         <h1>Trouvez le magasin qui vous correspond</h1>
 
-        <p>Choisissez un magasin pour voir ses produits disponibles.</p>
+        <p>
+            Choisissez un magasin pour voir ses produits disponibles.
+        </p>
+
+        <StoreHeaderNav />
     </header>
 </template>
---
+++ resources/js/Components/Front/StoreSelection/StoreHeaderNav.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { Link, usePage } from '@inertiajs/vue3';
+
+const page = usePage();
+</script>
+
+<template>
+    <nav class="store-header-nav" aria-label="Navigation principale">
+        <ul class="store-header-nav__list">
+            <li>
+                <Link href="/">Accueil</Link>
+            </li>
+
+            <template v-if="page.props.auth?.user">
+                <li>
+                    <Link href="/profile">Mon profil</Link>
+                </li>
+
+                <li>
+                    <Link
+                        href="/logout"
+                        method="post"
+                        as="button"
+                        type="button"
+                    >
+                        Déconnexion
+                    </Link>
+                </li>
+            </template>
+
+            <template v-else>
+                <li>
+                    <Link href="/login">Connexion</Link>
+                </li>
+
+                <li>
+                    <Link href="/register">Inscription</Link>
+                </li>
+            </template>
+        </ul>
+    </nav>
+</template>
